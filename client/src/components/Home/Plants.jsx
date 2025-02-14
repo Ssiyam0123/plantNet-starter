7bc705daaaf0300872f3b8ef5db8f341c731cf7d
Ssiyam0123 <ssiyam563@gmail.com>
2025-02-15 05:49:35 +0600

    fetch data for plants done

--- client/src/components/Home/Plants.jsx
+++ client/src/components/Home/Plants.jsx
@@ -7,7 +7,7 @@ const Plants = ({plants}) => {
     <Container>
       <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
         {
-          plants?.map((plant)=><Card key={plant._id}/>)
+          plants?.map((plant)=><Card data={plant} key={plant._id}/>)
         }
       </div>
     </Container>
